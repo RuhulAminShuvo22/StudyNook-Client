369e8fcc5cfa8b93d86f88265cf0226c3422ef7b
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-05-27 01:30:07 +0600

    edit on register page

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,29 +1,5 @@
 
 
-// import dns from "node:dns";
-// dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// const client = new MongoClient(process.env.MONGODB_URI);
-
-// // 🔥 IMPORTANT
-// await client.connect();
-
-// const db = client.db("studynook");
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db, {
-//     client,
-//   }),
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-// });
-
 import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
@@ -44,55 +20,18 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
 
-  trustedOrigins: ["http://localhost:3000"],
+  // trustedOrigins: ["http://localhost:3000"],
 
-  secret: process.env.BETTER_AUTH_SECRET,
+  // secret: process.env.BETTER_AUTH_SECRET,
 
-  baseURL: process.env.BETTER_AUTH_URL,
+  // baseURL: process.env.BETTER_AUTH_URL,
 });
 
 
-// import dns from "node:dns";
-// dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-// import { betterAuth } from "better-auth";
-// import { MongoClient } from "mongodb";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// const uri = process.env.MONGODB_URI;
-
-// if (!uri) {
-//   throw new Error("অনুগ্রহ করে .env.local ফাইলে MONGODB_URI যুক্ত করুন।");
-// }
-
-// // Next.js ডেভেলপমেন্ট মোডে বারবার কানেকশন তৈরি হওয়া রোধ করতে গ্লোবাল ক্যাশ ব্যবহার করা হয়েছে
-// let client;
-// let clientPromise;
-
-// if (process.env.NODE_ENV === "development") {
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri);
-//     global._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = global._mongoClientPromise;
-// } else {
-//   client = new MongoClient(uri);
-//   clientPromise = client.connect();
-// }
-
-// // ডাটাবেজ কানেকশন নিশ্চিত করা
-// const connectedClient = await clientPromise;
-// const db = connectedClient.db("studynook");
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db),
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-
-//   trustedOrigins: ["http://localhost:3000"],
-//   secret: process.env.BETTER_AUTH_SECRET,
-//   baseURL: process.env.BETTER_AUTH_URL,
-// });
